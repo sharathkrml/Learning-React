0803cfe0f011ceeba00cf85ca10d90f68c1a1201
Sharath V <sharathkrml@gmail.com>
2021-08-07 15:56:03 +0530

    Named Export and default export of Stateless Functional components

---
+++ src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//function Greet(){
+//    return <h1></h1>
+//}
+const Greet = () =><h1>Hello Sharath 😄 </h1>
+export default Greet
+//export default allow us to import Greet function at any name
+//eg:- in App.js import MyComponent from './components/Greet'
+//named export
+
+export const NewGreet =()=><h1>Named Export 😏</h1>
